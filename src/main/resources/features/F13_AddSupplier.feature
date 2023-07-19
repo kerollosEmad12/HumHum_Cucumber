7a662b70f2a172b548c8f3c3872504e1d2787de6
KerollosEmad12 <kyrillosemad06@gmail.com>
2023-07-19 11:50:31 +0300

    HumHum Project

--- src/main/resources/features/F13_AddSupplier.feature
+++ src/main/resources/features/F13_AddSupplier.feature
@@ -27,3 +27,67 @@ Feature: Test admin automation functionality
       | fabrica    |           |
       | fabrica    | d         |
       | fabrica    | De        |
+
+  Scenario Outline: user could create a new supplier from admin (Email)
+    And Ingress the first name "fabrica" and last name "Dev"
+    And Ingress the EN company name "fabrica" and AR company name "فابريكا"
+    And Ingress the selling for
+    And Ingress the job title "sales"
+    And Ingress the valid email "<email>"
+    And Ingress the country code
+    And Ingress the mobile number
+    And Ingress the password "Fabrica123!" and confirm password "Fabrica123!"
+    Then The create new supplier is submitted "<result>"
+    Examples:
+      |          email         | result |
+      |m.elzoghbi24gmail.com   | failure|
+      | m.elzoghbi24@gmail     | failure|
+      |  @gmail.com            | failure|
+      | m.elzoghbi24@@gmail.com| failure|
+      | m.elzoghbi24@-gmail.com| failure|
+      | m.elzoghbi24@ gmail.com| failure|
+      | m.elzoghbi24@gmail.com | failure|
+
+  Scenario Outline: user could create a new supplier from admin (phone number)
+    And Insertion the first name "fabrica" and last name "Dev"
+    And Insertion the EN company name "fabrica" and AR company name "فابريكا"
+    And Insertion the selling for
+    And Insertion the job title "sales"
+    And Insertion the valid email
+    And Insertion the country code
+    And Insertion the mobile number "<phone_number>"
+    And Insertion the password "Fabrica123!" and confirm password "Fabrica123!"
+    Then user could created new supplier account submitted "<result>"
+    Examples:
+      | phone_number | result |
+      | 0125489355   | failure|
+      | 656587566882 | failure|
+      | 0103358asd97 | failure|
+      | 0103358477 5 | failure|
+      | 01095510979  | failure|
+
+  Scenario Outline: user could create a new supplier from admin (password and confirm password)
+    And user type the first name "fabrica" and last name "Dev"
+    And user type the EN company name "fabrica" and AR company name "فابريكا"
+    And user type the selling for
+    And user type the job title "sales"
+    And user type the valid email
+    And user type the country code
+    And user type the mobile number
+    And user type the password "<password>" and confirm password "<confirm_password>"
+    Then the created new supplier account submitted "<result>"
+    Examples:
+      |  password   | confirm_password| result  |
+      | fa123       |  Fabrica123!    |  failure|
+      | Fabrica     |  Fabrica123!    |  failure|
+      | fabrica123! |  Fabrica123!    |  failure|
+      | FABRICA123! |  Fabrica123!    |  failure|
+      | Fabrica123  |  Fabrica123!    |  failure|
+      | 12345678    |  Fabrica123!    |  failure|
+      | @#%*&^!@    |  Fabrica123!    |  failure|
+      | Fabrica 123!|  Fabrica123!    |  failure|
+      | Fabrica123! |                 |  failure|
+      | Fabrica123! |  Fabrica124!    |  failure|
+      | Fabrica123! |  fabrica123!    |  failure|
+      | Fabrica123! |  Fabrica 123!   |  failure|
+      | Fabrica123! |  kerolos123!    |  failure|
--- src/test/java/org/example/stepDefs/C14_AddSupplier.java
+++ src/test/java/org/example/stepDefs/C14_AddSupplier.java
@@ -152,4 +152,267 @@ public class C14_AddSupplier {
 
         soft.assertAll();
     }
+
+    @And("Ingress the first name {string} and last name {string}")
+    public void step17(String first_name, String last_name) throws InterruptedException {
+        supplier.First.sendKeys(first_name);
+        Thread.sleep(Duration.ofSeconds(3));
+
+        supplier.Last.sendKeys(last_name);
+        Thread.sleep(Duration.ofSeconds(3));
+    }
+
+    @And("Ingress the EN company name {string} and AR company name {string}")
+    public void step18(String EN, String AR) throws InterruptedException {
+        supplier.compNameEn.sendKeys(EN);
+        Thread.sleep(Duration.ofSeconds(3));
+
+        supplier.compNameAr.sendKeys(AR);
+        Thread.sleep(Duration.ofSeconds(3));
+    }
+
+    @And("Ingress the selling for")
+    public void step19() throws InterruptedException {
+        supplier.sell1.click();
+        Thread.sleep(Duration.ofSeconds(3));
+
+        js.executeScript("arguments[0].click();", supplier.sell2);
+        Thread.sleep(Duration.ofSeconds(1));
+    }
+
+    @And("Ingress the job title {string}")
+    public void step20(String title) throws InterruptedException {
+        supplier.title.sendKeys(title);
+        Thread.sleep(Duration.ofSeconds(1));
+    }
+
+    @And("Ingress the valid email {string}")
+    public void step21(String email) throws InterruptedException {
+        supplier.em.sendKeys(email);
+        Thread.sleep(Duration.ofSeconds(1));
+    }
+
+    @And("Ingress the country code")
+    public void step22() throws InterruptedException {
+        supplier.code1.click();
+        Thread.sleep(Duration.ofSeconds(3));
+
+        js.executeScript("arguments[0].click();", supplier.code2);
+        Thread.sleep(Duration.ofSeconds(3));
+    }
+
+    @And("Ingress the mobile number")
+    public void step23() throws InterruptedException {
+        Faker faker = new Faker();
+        String phoneNumber = faker.numerify("010########");
+        supplier.mobile.sendKeys(phoneNumber);
+        Thread.sleep(Duration.ofSeconds(3));
+    }
+
+    @And("Ingress the password {string} and confirm password {string}")
+    public void step24(String password, String confirm_password) throws InterruptedException {
+        supplier.Password.sendKeys(password);
+        Thread.sleep(Duration.ofSeconds(3));
+
+        supplier.confirmPass.sendKeys(confirm_password);
+        Thread.sleep(Duration.ofSeconds(3));
+    }
+
+    @Then("The create new supplier is submitted {string}")
+    public void step25(String result) {
+        supplier.Save.click();
+
+        //1- message content or equal "Validation Error"
+        SoftAssert soft = new SoftAssert();
+        String actualMsg = driver.findElement(By.id("swal2-title")).getText();
+        soft.assertTrue(actualMsg.contains("Validation Error"),
+                "actualMsg : "+actualMsg +" | "+ "expected Msg : "+"Validation Error"
+        );
+
+        if ("failure".equals(result)){
+            assertTrue(driver.findElement(By.id("swal2-title")).isDisplayed());
+        }
+
+        else {
+            assertTrue(driver.findElement(By.cssSelector("div>button[type=\"submit\"]")).isDisplayed());
+        }
+
+        soft.assertAll();
+    }
+
+    @And("Insertion the first name {string} and last name {string}")
+    public void step26(String first_name, String last_name) throws InterruptedException {
+        supplier.First.sendKeys(first_name);
+        Thread.sleep(Duration.ofSeconds(3));
+
+        supplier.Last.sendKeys(last_name);
+        Thread.sleep(Duration.ofSeconds(3));
+    }
+
+    @And("Insertion the EN company name {string} and AR company name {string}")
+    public void step27(String EN, String AR) throws InterruptedException {
+        supplier.compNameEn.sendKeys(EN);
+        Thread.sleep(Duration.ofSeconds(3));
+
+        supplier.compNameAr.sendKeys(AR);
+        Thread.sleep(Duration.ofSeconds(3));
+    }
+
+    @And("Insertion the selling for")
+    public void step28() throws InterruptedException {
+        supplier.sell1.click();
+        Thread.sleep(Duration.ofSeconds(3));
+
+        js.executeScript("arguments[0].click();", supplier.sell2);
+        Thread.sleep(Duration.ofSeconds(1));
+    }
+
+    @And("Insertion the job title {string}")
+    public void step29(String title) throws InterruptedException {
+        supplier.title.sendKeys(title);
+        Thread.sleep(Duration.ofSeconds(1));
+    }
+
+    @And("Insertion the valid email")
+    public void step30() throws InterruptedException {
+        Faker faker = new Faker();
+        globalEmail = faker.internet().emailAddress();
+        supplier.em.sendKeys(globalEmail);
+        Thread.sleep(Duration.ofSeconds(1));
+    }
+
+    @And("Insertion the country code")
+    public void step31() throws InterruptedException {
+        supplier.code1.click();
+        Thread.sleep(Duration.ofSeconds(3));
+
+        js.executeScript("arguments[0].click();", supplier.code2);
+        Thread.sleep(Duration.ofSeconds(3));
+    }
+
+    @And("Insertion the mobile number {string}")
+    public void step32(String phone_number) throws InterruptedException {
+        supplier.mobile.sendKeys(phone_number);
+        Thread.sleep(Duration.ofSeconds(3));
+    }
+
+    @And("Insertion the password {string} and confirm password {string}")
+    public void step33(String password, String confirm_password) throws InterruptedException {
+        supplier.Password.sendKeys(password);
+        Thread.sleep(Duration.ofSeconds(3));
+
+        supplier.confirmPass.sendKeys(confirm_password);
+        Thread.sleep(Duration.ofSeconds(3));
+    }
+
+    @Then("user could created new supplier account submitted {string}")
+    public void step34(String result) {
+        supplier.Save.click();
+
+        //1- message content or equal "Validation Error"
+        SoftAssert soft = new SoftAssert();
+        String actualMsg = driver.findElement(By.id("swal2-title")).getText();
+        soft.assertTrue(actualMsg.contains("Validation Error"),
+                "actualMsg : "+actualMsg +" | "+ "expected Msg : "+"Validation Error"
+        );
+
+        if ("failure".equals(result)){
+            assertTrue(driver.findElement(By.id("swal2-title")).isDisplayed());
+        }
+
+        else {
+            assertTrue(driver.findElement(By.cssSelector("div>button[type=\"submit\"]")).isDisplayed());
+        }
+
+        soft.assertAll();
+    }
+
+    @And("user type the first name {string} and last name {string}")
+    public void step35(String first_name, String last_name) throws InterruptedException {
+        supplier.First.sendKeys(first_name);
+        Thread.sleep(Duration.ofSeconds(3));
+
+        supplier.Last.sendKeys(last_name);
+        Thread.sleep(Duration.ofSeconds(3));
+    }
+
+    @And("user type the EN company name {string} and AR company name {string}")
+    public void step36(String EN, String AR) throws InterruptedException {
+        supplier.compNameEn.sendKeys(EN);
+        Thread.sleep(Duration.ofSeconds(3));
+
+        supplier.compNameAr.sendKeys(AR);
+        Thread.sleep(Duration.ofSeconds(3));
+    }
+
+    @And("user type the selling for")
+    public void step37() throws InterruptedException {
+        supplier.sell1.click();
+        Thread.sleep(Duration.ofSeconds(3));
+
+        js.executeScript("arguments[0].click();", supplier.sell2);
+        Thread.sleep(Duration.ofSeconds(1));
+    }
+
+    @And("user type the job title {string}")
+    public void step38(String title) throws InterruptedException {
+        supplier.title.sendKeys(title);
+        Thread.sleep(Duration.ofSeconds(1));
+    }
+
+    @And("user type the valid email")
+    public void step39() throws InterruptedException {
+        Faker faker = new Faker();
+        globalEmail = faker.internet().emailAddress();
+        supplier.em.sendKeys(globalEmail);
+        Thread.sleep(Duration.ofSeconds(1));
+    }
+
+    @And("user type the country code")
+    public void step40() throws InterruptedException {
+        supplier.code1.click();
+        Thread.sleep(Duration.ofSeconds(3));
+
+        js.executeScript("arguments[0].click();", supplier.code2);
+        Thread.sleep(Duration.ofSeconds(3));
+    }
+
+    @And("user type the mobile number")
+    public void step41() throws InterruptedException {
+        Faker faker = new Faker();
+        String phoneNumber = faker.numerify("010########");
+        supplier.mobile.sendKeys(phoneNumber);
+        Thread.sleep(Duration.ofSeconds(3));
+    }
+
+    @And("user type the password {string} and confirm password {string}")
+    public void step42(String password, String confirm_password) throws InterruptedException {
+        supplier.Password.sendKeys(password);
+        Thread.sleep(Duration.ofSeconds(3));
+
+        supplier.confirmPass.sendKeys(confirm_password);
+        Thread.sleep(Duration.ofSeconds(3));
+    }
+
+    @Then("the created new supplier account submitted {string}")
+    public void step43(String result) {
+        supplier.Save.click();
+
+        //1- message content or equal "Validation Error"
+        SoftAssert soft = new SoftAssert();
+        String actualMsg = driver.findElement(By.id("swal2-title")).getText();
+        soft.assertTrue(actualMsg.contains("Validation Error"),
+                "actualMsg : "+actualMsg +" | "+ "expected Msg : "+"Validation Error"
+        );
+
+        if ("failure".equals(result)){
+            assertTrue(driver.findElement(By.id("swal2-title")).isDisplayed());
+        }
+
+        else {
+            assertTrue(driver.findElement(By.cssSelector("div>button[type=\"submit\"]")).isDisplayed());
+        }
+
+        soft.assertAll();
+    }
 }
